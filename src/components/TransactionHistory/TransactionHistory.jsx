28f546a72755af71fa8ce7b0b222bea8f6ed9c02
vokur13 <vokur13@gmail.com>
2022-09-20 14:36:54 +0300

    TransactionHistory styled

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,29 +1,32 @@
 import PropTypes from 'prop-types';
 import css from './TransactionHistory.module.css';
+import commonStyles from '../../css/Common.module.css';
 
 export const TransactionHistory = ({ title, items }) => {
   return (
-    <section className={css.section}>
-      {title && <h2 className={css.title}>{title}</h2>}
-      <table className={css.transactionHistory}>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {items.map(item => (
-            <tr key={item.id}>
-              <td>{item.type}</td>
-              <td>{item.amount}</td>
-              <td>{item.currency}</td>
+    <section className={commonStyles.section}>
+      {title && <h2 className={commonStyles.title}>{title}</h2>}
+      <div className={commonStyles.container}>
+        <table className={css.transactionHistory}>
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>Amount</th>
+              <th>Currency</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {items.map(item => (
+              <tr key={item.id}>
+                <td className={css.itemType}>{item.type}</td>
+                <td>{item.amount}</td>
+                <td>{item.currency}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 };
