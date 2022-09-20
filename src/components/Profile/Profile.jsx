ac4a0c09e3c18cc50d5edbda21b5ef4078a9e09f
vokur13 <vokur13@gmail.com>
2022-09-20 12:19:30 +0300

    profile section styled

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { FaMapMarkerAlt, FaUserAlt } from 'react-icons/fa';
 import { iconSize } from 'constants';
 import css from './Profile.module.css';
+import commonStyles from '../../css/Common.module.css';
 
 export const Profile = ({
   username,
@@ -11,35 +12,39 @@ export const Profile = ({
   stats,
 }) => {
   return (
-    <div className={css.profile}>
-      <div className={css.description}>
-        <img
-          src={avatar ?? FaUserAlt}
-          alt="User avatar"
-          className={css.avatar}
-        />
-        <p className={css.name}>{username}</p>
-        <p className={css.tag}>{'@' + tag}</p>
-        <p className={css.location}>
-          <FaMapMarkerAlt className={css.icon} size={iconSize.s} />
-          {location}
-        </p>
-      </div>
+    <div className={commonStyles.section}>
+      <div className={css.profile}>
+        <div className={commonStyles.container}>
+          <div className={css.description}>
+            <img
+              src={avatar ?? FaUserAlt}
+              alt="User avatar"
+              className={css.avatar}
+            />
+            <p className={css.name}>{username}</p>
+            <p className={css.tag}>{'@' + tag}</p>
+            <p className={css.location}>
+              <FaMapMarkerAlt className={css.icon} size={iconSize.s} />
+              {location}
+            </p>
+          </div>
 
-      <ul className={css.stats}>
-        <li>
-          <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
-        </li>
-        <li>
-          <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
-        </li>
-        <li>
-          <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
-        </li>
-      </ul>
+          <ul className={css.stats}>
+            <li>
+              <span className={css.label}>Followers</span>
+              <span className={css.quantity}>{stats.followers}</span>
+            </li>
+            <li>
+              <span className={css.label}>Views</span>
+              <span className={css.quantity}>{stats.views}</span>
+            </li>
+            <li>
+              <span className={css.label}>Likes</span>
+              <span className={css.quantity}>{stats.likes}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
